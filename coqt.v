14ac72caa4a25d137f366a7c94dd117cff014bf0
WATANABE Yuichi <nabe.gm@gmail.com>
2013-12-02 07:20:57 +0900

    Add function id

--- coqt.v
+++ coqt.v
@@ -4,3 +4,6 @@ Eval compute in plus 3 5.
 
 Definition plus' : nat -> nat -> nat := fun n m => n + m.
 Eval compute in plus' 3 5.
+
+Definition id (A : Type)(x : A) : A := x.
+Eval compute in id nat 5.
