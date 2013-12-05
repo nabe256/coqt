7f6afca78d9ab40d3d0b9352b4dbcb50d44d429a
WATANABE Yuichi <nabe.gm@gmail.com>
2013-12-05 10:42:34 +0900

    Add exercise ex1

--- coqt.v
+++ coqt.v
@@ -19,3 +19,6 @@ Definition prop1 : forall (A B C : Prop), (B -> C) -> (A -> B) -> (A -> C) :=
 
 Definition ex0 : forall (A B : Prop), A -> (A -> B) -> B :=
   fun A B x f => f x.
+
+Definition ex1 : forall (A B C : Prop), (A -> B -> C) -> (B -> A -> C) :=
+  fun A B C f g h => f h g.
