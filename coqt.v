a1a6feb756371e1af0bdb6b3e9773eeb244572cf
WATANABE Yuichi <nabe.gm@gmail.com>
2013-12-12 01:23:23 +0900

    Add theorem app_assoc : Associative law

--- coqt.v
+++ coqt.v
@@ -210,3 +210,15 @@ Proof.
   f_equal. (* apply (f_equal (cons a)). *)
   apply IHl.
 Qed.
+
+Theorem app_assoc : forall (A : Type)(l1 l2 l3 : list A), l1 ++ (l2 ++ l3) = (l1 ++ l2) ++ l3.
+Proof.
+  intros.
+  induction l1.
+
+  reflexivity.
+
+  simpl.
+  f_equal.
+  apply IHl1.
+Qed.
